a3ecc00db5a504a39ca4456dc9e56023eec3276b
Aakash Shah <aadi1428@gmail.com>
2018-08-15 15:41:19 -0400

    Min Heap K-Sorted Array Done

--- src/main/java/KMessedArray/KMessedArraySort.java
+++ src/main/java/KMessedArray/KMessedArraySort.java
@@ -1,15 +1,18 @@
 package main.java.KMessedArray;
 
+import java.util.PriorityQueue;
+
 public class KMessedArraySort {
 
 	
 	public static void main(String[] args) {
-		    int[] arr = {1, 4, 5, 200, 3, 7, 8, 6, 10, 9};
+		    int[] arr = {1, 4, 5, 2, 3, 7, 8, 6, 10, 9};
 		    System.out.println(" ");
 
 		    for(int i: arr){
 		      System.out.print(i+ " ");
 		    }
+		    
 		    int k = 2;
 		    int[] result = sortKMessedArray(arr,k);
 		    
@@ -20,47 +23,87 @@ public class KMessedArraySort {
 		    }
 	 }
 
-	/**
-	 * @param arr
-	 * @param k
-	 * @return
-	 */
-	private static int[] sortKMessedArray(int[] arr, int k) {
-	
-		/*
-		 * Insertion Sort. Naive approach. 
-		 */
+//	private static int[] sortKMessedArray(int[] arr, int k) {
+//		/*
+//		 * Insertion Sort
+//		 * Time complexity -> O(n.k)
+//		 * Since each neighbor will only be k positions far away. The second loop will never exceed k iterations. Hence O(n.k)
+//		 * Although we do not use the value of k.
+//		 */
 //		int prev = 0;
 //		for(int i=1;i<arr.length;i++) {
 //			prev = i-1;
 //			if(arr[i] < arr[prev]) {
-//				swap(arr,i,prev);
-//				int dup_i = i-1;
-//				for(int j=prev-1;j>=0;j--) {
-//					if(arr[dup_i] < arr[j]) {
-//						swap(arr,dup_i,j);
+//				for(int j=prev;j>=0;j--) {
+//					if(arr[i] < arr[j]) {
+//						swap(arr,i,j);
 //					}
-//					dup_i--;
+//					i--;
 //				}
-//			}else {continue;}
+//			}
 //		}
+//		return arr;
+//	}
+	
+	
+	private static int[] sortKMessedArray(int[] arr, int k) {
 		
+		int n = arr.length;
+		/*
+//		 * Using Min Heap
+//		 * Time complexity -> O(n log(k))
+//		 * Essentially linear. The one pass of the array and thr log(k) heap will extract_min() n times.
+		 */
 		
-		int prev = 0;
-		for(int i=1;i<arr.length;i++) {
-			prev = i-1;
-			if(arr[i] < arr[prev]) {
-				for(int j=prev;j>=0;j--) {
-					if(arr[i] < arr[j]) {
-						swap(arr,i,j);
-					}
-					i--;
-				}
-			}
+		PriorityQueue<Integer> heap = new PriorityQueue<Integer>();
+		
+		/*
+		 * {1, 4, 5, 2, 3, 7, 8, 6, 10, 9} , k = 2;
+		 * First k+1 elements in the heap.
+		 * 0,1,2,3 <k+1> in heap.
+		 */
+		
+		for(int i=0; i<=k; i++) {
+			heap.add(arr[i]);
+		}
+		
+		/*
+		 * {1, 4, 5, 2, 3, 7, 8, 6, 10, 9} , k = 2, n = 10;
+		 *  Traverse the rest of the elements 
+		 *  i From k+1 to n-1     -> 3 4 5 6 7 8 9
+		 *  Array Positions       -> 0 1 2 3 4 5 6
+		 *  Formula for idx = i-k -> 1 2 3 4 5 6 7
+		 *  Formula idx =   i-k-1 -> 0 1 2 3 4 5 6
+		 *  
+		 *  Extract minimum so far and add it to the arr. 
+		 *  Pass the current element in the heap. 
+		 *  
+		 *  Thus the heap keeps taking one new number every pass
+		 *  and extracting the minimum so far which we can put it in the array.
+		*/
+		
+		for(int i=k+1; i<n ; i++) {
+			arr[i-k-1] = heap.poll();
+			heap.add(arr[i]);
+		}
+		
+		/*
+		 *  {1, 2, 3, 4, 5, 6, 7 | 6, 10, 9 } , k = 2, n = 10;
+		 *  Traverse the rest of the elements 
+		 *  i From 0 to k           -> 0,    1,    2
+		 *  Array Positions         -> 7,    8,    9
+		 *  Formula for idx n-(k+1) -> 7+0, 7+1, 7+2
+		 *  
+		 *  Add the remaining values from the heap. These are the k+1 values we missed above.
+		*/
+		
+		for(int i=0; i<=k; i++) {
+			arr[n-k-1 + i] = heap.poll();
 		}
 		
 		return arr;
 	}
+	
 	
 	public static void swap(int[] arr, int i, int j) {
 		int temp = arr[i];
